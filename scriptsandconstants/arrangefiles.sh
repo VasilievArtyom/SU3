2b69afc3466a469531ca6d13597408aa473e5f0a
VasilievAtyom <artyom.vasiliev.1@gmail.com>
2019-11-30 16:05:16 +0300

    add results

--- scriptsandconstants/arrangefiles.sh
+++ scriptsandconstants/arrangefiles.sh
@@ -4,15 +4,15 @@
 projpath=/home/vasiliev_artyom/proj/SU3_stag/bin/SU3_stag
 
 
-for Ls in 8 10 12
+for Ls in 24 
 do
-for Lt in 8 10 12
+for Lt in 12
 do
-for ma in 0.01 0.0001
+for ma in 0.01
 do
-for mu in 0.00 0.10 0.20 0.30 0.40 0.50 0.60 0.70 
+for mu in 0.05 
 do
-for nfluxes in {0..32}
+for nfluxes in 0
 do
 logspath=/home/vasiliev_artyom/_scratch/compute/SU3/logs/${Ls}^3x${Lt}/mu=${mu}/ma=${ma}/nfluxes=${nfluxes}
 confpath=/home/vasiliev_artyom/_scratch/compute/SU3/configurations/${Ls}^3x${Lt}/mu=${mu}/ma=${ma}/nfluxes=${nfluxes}
