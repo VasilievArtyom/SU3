05e86e2386eeed1b03bb2cd06aa016aa8b802da9
VasilievAtyom <artyom.vasiliev.1@gmail.com>
2019-12-04 20:54:52 +0300

    add results

--- scriptsandconstants/MeanAndError.sh
+++ scriptsandconstants/MeanAndError.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-
-projpath=/home/vasiliev_artyom/proj/SU3_stag/bin/SU3_stag
-rm AxialCurrent.txt
-echo "#Ls   Lt  mu  ma  nfluxes AxialCurrent Err" >> AxialCurrent.txt
-
-for Ls in 12 16 24
+for Ls in 8
 do
-for Lt in 12 16 24
+for Lt in 8
 do
-for mu in 0.00 0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75
+for mu in 0.00 0.10 0.20 0.30 0.40 0.50 0.60 0.70
 do
-for ma in 0.001
+for ma in 0.01 0.0001
 do
-for nfluxes in {0..50}
+for nfluxes in {0..32}
 do
 logspath=/home/vasiliev_artyom/_scratch/compute/SU3/logs/${Ls}^3x${Lt}/mu=${mu}/ma=${ma}/nfluxes=${nfluxes}
 
@@ -44,5 +39,3 @@ done
 done
 done
 done
-
-#echo "${Ls} ${Lt} ${mu} ${ma} ${nfluxes} $(cat xx04 | head -2 | tail -1)" >> AxialCurrent.txt
