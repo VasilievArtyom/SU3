672a8f8fd1d857244a0013e997904c485bf4b95f
VasilievAtyom <artyom.vasiliev.1@gmail.com>
2020-02-21 00:44:04 +0300

    add results

--- scriptsandconstants/calcsctipts/task39.sh
+++ scriptsandconstants/calcsctipts/task39.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./home/vasiliev_artyom/proj/SU3_stag/bin/SU3_stag -G0 -L/home/vasiliev_artyom/_scratch/compute/SU3/logs/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 -O/home/vasiliev_artyom/_scratch/compute/SU3/configurations/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 -D/home/vasiliev_artyom/_scratch/compute/SU3/output/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 -Nconf_gen -C/home/vasiliev_artyom/_scratch/compute/SU3/scriptsandconstants/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32/constants.txt -A
-echo 2137{Ls}^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 >> calclog.txt
-./home/vasiliev_artyom/_scratch/compute/SU3/scriptsandconstants/BU.sh
+nohup ./home/vasiliev_artyom/proj/SU3_stag/bin/SU3_stag -G0 -L/home/vasiliev_artyom/_scratch/compute/SU3/logs/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 -O/home/vasiliev_artyom/_scratch/compute/SU3/configurations/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 -D/home/vasiliev_artyom/_scratch/compute/SU3/output/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 -Nconf_gen -C/home/vasiliev_artyom/_scratch/compute/SU3/scriptsandconstants/24^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32/constants.txt -A
+echo 3443{Ls}^3x6/mu=0.14399/m_sa=0.136957/nfluxes=32 >> calclog.txt
+. /home/vasiliev_artyom/_scratch/compute/SU3/scriptsandconstants/BU.sh
